06add2158cb67c350e6831efc355ae25e95976ca
ructcr <280354646@qq.com>
2020-12-03 23:38:55 +1100

    trytry

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -12,6 +12,7 @@ class Network(object):
             random.shuffle(training_data)
             mini_batches=[training_data[k:k+mini_batch_size] for k in range(0,n,mini_batch_size)]
             for mini_batch in mini_batches:
+                print('minibatch:',mini_batch)
                 self.update_mini_batch(mini_batch,eta)
             if test_data:
                 print ("Epoch {0}:{1}/{2}".format(j,self.evaluate(test_data),n_test))
@@ -22,12 +23,17 @@ class Network(object):
         nabla_w=[np.zeros(w.shape) for w in self.weights]
         #print(nabla_b,'\n',nabla_w)
         for x,y in mini_batch:
+            #print('x:',x,' y:',y)
             delta_nabla_b,delta_nabla_w=self.backprop(x,y)
-            #print(delta_nabla_b,'\n',delta_nabla_w)
+            #print('deltas:',delta_nabla_b,delta_nabla_w)
             """ nabla_b=[nb+dnb for nb,dnb in zip(nabla_b,delta_nabla_b)]
             nabla_w=[nw+dnw for nw,dnw in zip(nabla_w,delta_nabla_w)]
             self.weights=[w-(eta/len(mini_batch))*nw for w,nw in zip(self.weights,nabla_w)]
             self.biases=[b-(eta/len(mini_batch))*nb for b,nb in zip(self.biases,nabla_b)] """
+    def feedfoward(self,a):
+        for b,w in zip(self.biases,self.weights):
+            a=self.sigmoid(np.dot(a,w)+b)
+        return a
     def backprop(self,x,y):
         nabla_b=[np.zeros(b.shape) for b in self.biases]
         nabla_w=[np.zeros(w.shape) for w in self.weights]
@@ -35,9 +41,25 @@ class Network(object):
         activations=[x]
         zs=[]
         for b,w in zip(self.biases,self.weights):
-            print(b,'\n\n',w,'\n\n\n')
-            z=np.dot(w,activation)+b
+            #print(b,'\n\n',w,'\n\n\n')
+            z=np.dot(activation,w)+b
+            zs.append[z]
+            activation=self.sigmoid(z)
+            activations.append(activation)             
+            #print(activation)
+        delta=self.cost_derivative(activations[-1],y)*self.sigmoid_prime(zs[-1])
+        nabla_b[-1]=delta
+        nabla_w[-1]=np.dot(delta,activations[-2].transpose())
         return(nabla_b,nabla_w)
+    def cost_derivative(self,output_activations,y):
+        return(output_activations-y)
+    def evaluate(self,test_data):
+        test_results=[(np.argmax(self.feedfoward(x)),y) for (x,y) in test_data]
+        return sum(int(x==y) for (x,y) in test_results)
+    def sigmoid(self,z):
+        return 1.0/(1.0+np.exp(-z))
+    def sigmoid_prime(self,z):
+        return sigmoid(z)*(1-sigmoid(z))
 
 net=Network([3,5,2])
 net.SGD([([0,0,1],1),([0,1,0],0),([1,0,0],1)],1,3,0.3)
